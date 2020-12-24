74e3f84ed1cc3e296d635e0dd7fb38264afc7b09
David Bestz <75372386+Davoe-D@users.noreply.github.com>
2020-12-24 15:20:54 +0100

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,11 +4,18 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/naijabestz")],
-        [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/BestzBrothers")]
-    ])
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('❤ Bots Updates Channel ❤', url='https://t.me/naijabestz'),
+                    InlineKeyboardButton('Feedback 🤷‍♂️', url='https://t.me/bestzbrothers')
+                ],
+               [
+                    InlineKeyboardButton('⭐ Support Group ⭐', url='https://t.me/naija_bestz'),
+                    InlineKeyboardButton('Source 😜', url='https://github.com/Davoe-D/TheClickFly')
+                ]
+            ]
+        )
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
